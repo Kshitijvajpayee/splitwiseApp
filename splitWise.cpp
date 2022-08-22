c22bb413df011b845a9de091474d49ffa12b23f8
Kshitij Vajpayee <bkshitij189@gmail.com>
2022-08-23 00:22:33 +0530

    splitwise mindset

---
+++ splitWise.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <set>
+#include <map>
+using namespace std;
+// Money Splitwise Algorithm
+
+/*
+Input:
+3 3
+Ravi Aman 100
+Aman Arpita 50
+Arpita Ravi 40
+
+
+Output - Should also show transactions
+Ravi pays 50 to Aman
+Ravi pays 10 to Arpita
+
+cnt=2
+*/
+
+class person_compare
+{
+public:
+    bool operator()(pair<string, int> p1, pair<string, int> p2)
+    {
+        return p1.second < p2.second;
+    }
+};
+
+int main()
+{
+
+    int noOfTransactions, friends;
+    cin >> noOfTransactions >> friends;
+
+    string x, y;
+    int amount;
+
+    map<string, int> net;
+
+    while (noOfTransactions--)
+    {
+        cin >> x >> y >> amount;
+        if (net.count(x) == 0)
+        {
+            net[x] = 0;
+        }
+        if (net.count(y) == 0)
+        {
+            net[y] = 0;
+        }
+        net[x] -= amount;
+        net[y] += amount;
+    }
+
+    // Iterate over the persons, add those person in the multiset who have non zero net
+
+    multiset<pair<string, int>, person_compare> m;
+
+    for (auto p : net)
+    {
+        string person = p.first;
+        int amount = p.second;
+
+        if (net[person] != 0)
+        {
+            m.insert(make_pair(person, amount));
+        }
+    }
+
+    // 3. settlements (start & end)
+    int cnt = 0;
+    while (!m.empty())
+    {
+        auto low = m.begin();
+        auto high = prev(m.end());
+
+        int debit = low->second;
+        string debit_person = low->first;
+
+        int credit = high->second;
+        string credit_person = high->first;
+
+        // Pop them out
+        m.erase(low);
+        m.erase(high);
+
+        int settled_amount = min(-debit, credit);
+        debit += settled_amount;
+        credit -= settled_amount;
+
+        // Print the Transaction between people
+        cout << debit_person << " will pay " << settled_amount << " to " << credit_person << endl;
+
+        if (debit != 0)
+        {
+            m.insert(make_pair(debit_person, debit));
+        }
+        if (credit != 0)
+        {
+            m.insert(make_pair(credit_person, credit));
+        }
+        cnt += 1;
+    }
+    cout << "count=" << cnt << endl;
+    return 0;
+}
